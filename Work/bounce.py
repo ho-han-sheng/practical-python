d547ea63e70190e78aafa9f65f4cd59efacc976b
Han Sheng <hohansheng@gmail.com>
2023-01-28 08:02:45 +0800

    complete exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+bounce_limit = int(input('Input the number of bounces:'))
+x = 1
+
+while x < bounce_limit + 1:
+    height *= 0.6
+    print(round(height, 4))
+    x += 1
---
+++ Work/stock.py
@@ -0,0 +1,10 @@
+# stock.py
+
+class stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def cost():
+        
